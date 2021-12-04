b42af0768937210b12c0b1b3fc99236430c542d0
Nanda <nanda.s@hey.com>
2021-12-04 12:32:34 -0800

    feat: add new problem script and day 4 template

---
+++ 4/prepare.js
@@ -0,0 +1,2 @@
+export default (file) => file.trim().split(/?
+/);
---
+++ new.js
@@ -0,0 +1,17 @@
+import * as fs from "fs";
+
+const day = Number(process.argv[2]);
+
+const files = {
+  [`${day}a.js`]: `export default () => {}`,
+  [`${day}b.js`]: `export default () => {}`,
+  "example.txt": ``,
+  "input.txt": ``,
+  "prepare.js": `export default (file) => file.trim().split(/\r?\n/);`,
+  "README.md": `# Day ${day}`,
+};
+
+fs.mkdirSync(String(day));
+for (const [path, contents] of Object.entries(files)) {
+  fs.promises.writeFile(`${day}/${path}`, contents);
+}

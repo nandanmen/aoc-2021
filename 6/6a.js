25e0f1fff854c3044694bc239dce9a5e582dc988
Nanda Syahrasyad <nanda.s@hey.com>
2021-12-29 17:08:23 -0800

    feat: add back naive solution

--- 6/6a.js
+++ 6/6a.js
@@ -1,45 +1,18 @@
-const DAYS = 256;
-const NEW_FISH_TIME = 7;
+const DAYS = 80;
+const NEW_FISH_TIME = 6;
 const NEW_FISH_DELAY = 2;
 
-const cache = {};
-let hits = 0;
-let misses = 0;
-
 export default (...fishes) => {
-  let fishCount = 0;
-
-  fishes.forEach((start) => {
-    fishCount += getValueAfterDays(start, DAYS);
-  });
-
-  console.log("hits: ", hits);
-  console.log("misses: ", misses);
-
-  return fishCount;
-};
-
-const getValueAfterDays = (start, days) => {
-  if (cache[`${start}-${days}`]) {
-    hits++;
-    return cache[`${start}-${days}`];
+  for (let i = 0; i < DAYS; i++) {
+    for (const [index, fish] of fishes.entries()) {
+      if (!fish) {
+        fishes[index] = NEW_FISH_TIME;
+        fishes.push(NEW_FISH_TIME + NEW_FISH_DELAY + 1);
+      } else {
+        fishes[index]--;
+      }
+    }
   }
 
-  misses++;
-
-  const fishesCreated = Math.max(Math.ceil((days - start) / NEW_FISH_TIME), 0);
-
-  let subFishesCreated = 0;
-
-  for (let offset = 0; offset < fishesCreated; offset++) {
-    const fishLifetime = days - start - NEW_FISH_TIME * offset - 1;
-    subFishesCreated += getValueAfterDays(
-      NEW_FISH_TIME + NEW_FISH_DELAY - 1,
-      fishLifetime
-    );
-  }
-
-  const fishes = 1 + subFishesCreated;
-  cache[`${start}-${days}`] = fishes;
-  return fishes;
+  return fishes.length;
 };
--- 6/6b.js
+++ 6/6b.js
@@ -1,1 +1,45 @@
-export default () => {}
+const DAYS = 256;
+const NEW_FISH_TIME = 7;
+const NEW_FISH_DELAY = 2;
+
+const cache = {};
+let hits = 0;
+let misses = 0;
+
+export default (...fishes) => {
+  let fishCount = 0;
+
+  fishes.forEach((start) => {
+    fishCount += getValueAfterDays(start, DAYS);
+  });
+
+  console.log("hits: ", hits);
+  console.log("misses: ", misses);
+
+  return fishCount;
+};
+
+const getValueAfterDays = (start, days) => {
+  if (cache[`${start}-${days}`]) {
+    hits++;
+    return cache[`${start}-${days}`];
+  }
+
+  misses++;
+
+  const fishesCreated = Math.max(Math.ceil((days - start) / NEW_FISH_TIME), 0);
+
+  let subFishesCreated = 0;
+
+  for (let offset = 0; offset < fishesCreated; offset++) {
+    const fishLifetime = days - start - NEW_FISH_TIME * offset - 1;
+    subFishesCreated += getValueAfterDays(
+      NEW_FISH_TIME + NEW_FISH_DELAY - 1,
+      fishLifetime
+    );
+  }
+
+  const fishes = 1 + subFishesCreated;
+  cache[`${start}-${days}`] = fishes;
+  return fishes;
+};

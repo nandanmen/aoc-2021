688466dc4d460f108c5623a68094a946ce6b729c
Nanda Syahrasyad <nanda.s@hey.com>
2022-12-16 12:48:54 -0800

    feat: add 2022/6

---
+++ 2022/6/6a.js
@@ -0,0 +1,20 @@
+const NUM_UNIQUE_CHARS = 4;
+
+export default (signal) => {
+  let windowStart = 0;
+  let windowEnd = NUM_UNIQUE_CHARS;
+
+  while (windowEnd < signal.length) {
+    const window = signal.slice(windowStart, windowEnd);
+    const uniqueChars = new Set(window);
+
+    if (uniqueChars.size === NUM_UNIQUE_CHARS) {
+      return windowEnd;
+    }
+
+    windowStart++;
+    windowEnd++;
+  }
+
+  return windowEnd;
+};
